4b7eb623101ff35430d8a9229db10012475e1119
Elvis Knapman <elvis@elvisknapman.com>
2019-09-05 12:41:30 -0400

    environment stuff

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const helmet = require('helmet');
+require("dotenv").config();
 
-const apiRouter = require('./api-router.js');
+const express = require("express");
+const helmet = require("helmet");
+
+console.log("environment", process.env.NODE_ENV);
+
+const apiRouter = require("./api-router.js");
 
 const server = express();
 
 server.use(helmet());
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
 
 module.exports = server;
